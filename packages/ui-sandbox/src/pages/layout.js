e3d793d56351fd0c76d618ead01447998a169064
denny <denny.george90@gmail.com>
2019-11-08 17:50:44 -0800

    dmp

--- packages/ui-sandbox/src/pages/layout.js
+++ packages/ui-sandbox/src/pages/layout.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Grommet, ResponsiveContext, Box, Stack, Grid, Heading, Text} from 'grommet'
+import {Grommet, ResponsiveContext, Box, Stack, Layer, Grid, Button, Heading, Text} from 'grommet'
 import {Layout} from '@tattle-made/ui';
 const {LayoutPortal, AppShell} = Layout;
 
@@ -12,24 +12,45 @@ const {LayoutPortal, AppShell} = Layout;
 const LayoutPage = () => {
     return (
         <AppShell>
-
-                <LayoutPortal
-                    primaryNavigationContent={
-                        <Heading> Navigation </Heading>
-                    }
-                    mainSectionContent={
-                        <>
-                        <Heading> Main </Heading>
-                        <Text>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed diam massa, varius quis faucibus vel, eleifend eu leo. Donec felis massa, semper in massa sit amet, eleifend facilisis nulla. Nunc lacinia, sapien a tincidunt pellentesque, odio libero feugiat ligula, vel dictum lorem diam quis leo. Duis placerat a justo non tempor. Quisque aliquet felis ac nibh vulputate, et condimentum arcu consectetur. Sed ac risus fringilla, mattis lectus sed, porttitor libero. Praesent mattis eros mi, at condimentum metus congue sit amet. Aenean aliquam pharetra purus id lacinia.
-                            Fusce condimentum posuere est id luctus. Maecenas sed sem iaculis, euismod justo ut, scelerisque dolor. Quisque et malesuada sapien. Fusce tortor tortor, commodo non rutrum in, pulvinar at risus. Nunc enim leo, elementum et libero sit amet, dictum efficitur arcu. In tempor sodales lectus et laoreet. Donec dapibus tincidunt urna nec luctus. Aliquam erat volutpat. Phasellus nec nibh consequat, molestie est ut, tristique mi. 
-                        </Text>
-                        </>
-                    }
+            <LayoutPortal
+                primaryNavigationContent={
+                    <Heading> Navigation </Heading>
+                }
+                mainSectionContent={
+                    <>
+                    <Heading> Main </Heading>
+                    <Text>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed diam massa, varius quis faucibus vel, eleifend eu leo. Donec felis massa, semper in massa sit amet, eleifend facilisis nulla. Nunc lacinia, sapien a tincidunt pellentesque, odio libero feugiat ligula, vel dictum lorem diam quis leo. Duis placerat a justo non tempor. Quisque aliquet felis ac nibh vulputate, et condimentum arcu consectetur. Sed ac risus fringilla, mattis lectus sed, porttitor libero. Praesent mattis eros mi, at condimentum metus congue sit amet. Aenean aliquam pharetra purus id lacinia.
+                        Fusce condimentum posuere est id luctus. Maecenas sed sem iaculis, euismod justo ut, scelerisque dolor. Quisque et malesuada sapien. Fusce tortor tortor, commodo non rutrum in, pulvinar at risus. Nunc enim leo, elementum et libero sit amet, dictum efficitur arcu. In tempor sodales lectus et laoreet. Donec dapibus tincidunt urna nec luctus. Aliquam erat volutpat. Phasellus nec nibh consequat, molestie est ut, tristique mi. 
+                    </Text>
+                    </>
+                }
+            >
+            </LayoutPortal>
+            
+            <Layer
+                position="bottom"
+                modal={false}
+                margin={{ vertical: "medium", horizontal: "small" }}
+                responsive={false}
+                plain
                 >
-                </LayoutPortal>
-
-
+                <Box
+                    align="center"
+                    direction="row"
+                    gap="small"
+                    justify="between"
+                    round="medium"
+                    elevation="medium"
+                    pad={{ vertical: "xsmall", horizontal: "small" }}
+                    background="status-ok"
+                >
+                    <Box align="center" direction="row" gap="xsmall">
+                        <Text>A new virtual machine has been successfully added</Text>
+                    </Box>
+                    <Button plain> x </Button>
+                </Box>
+            </Layer>
         </AppShell>
     )
 }
--- packages/ui/src/components/layout/LayoutPortal.js
+++ packages/ui/src/components/layout/LayoutPortal.js
@@ -2,69 +2,43 @@ import React from 'react'
 import {Grommet, ResponsiveContext, Box, Stack, Grid, Heading, Text} from 'grommet'
 import TattleTheme from '../../theme'
 
-const PortalLayoutDesktop = ({primaryNavigationContent, mainSectionContent, children}) => {
-  return(
-      <Grid
-          rows={['flex']}
-          columns={['auto', 'flex']}
-          areas={[
-              {name: 'PrimaryNavigationSection', start:[0,0], end:[0,0]},
-              {name: 'MainSection', start:[1,0], end:[1,0]}
-          ]}
-          gap={'small'}
-          fill
-      >
-      <Box fill background={'brand-1'} gridArea={'PrimaryNavigationSection'}>
-          {primaryNavigationContent}
-      </Box>
-      <Box fill background={'light-2'} gridArea={'MainSection'}>
-          {mainSectionContent}
-      </Box>
-      </Grid>
-  )   
+const columns = {
+    'medium' : ['auto', 'flex'],
+    'small' : ['xsmall', 'flex']
 }
 
-const PortalLayoutMobile = ({primaryNavigationContent, mainSectionContent, children}) => {
-  return(
-      <Grid
-          rows={['flex']}
-          columns={['xsmall', 'flex']}
-          areas={[
-              {name: 'PrimaryNavigationSection', start:[0,0], end:[0,0]},
-              {name: 'MainSection', start:[1,0], end:[1,0]}
-          ]}
-          gap={'small'}
-          fill
-      >
-      <Box fill background={'brand-1'} gridArea={'PrimaryNavigationSection'}>
-          {primaryNavigationContent}
-      </Box>
-      <Box fill background={'light-2'} gridArea={'MainSection'}>
-          {mainSectionContent}
-      </Box>
-      </Grid>
-  )   
+const PortalGrid = ({size, primaryNavigationContent, mainSectionContent}) => {
+    return(
+        <Grid
+            rows={['flex']}
+            columns={columns[size]}
+            areas={[
+                {name: 'PrimaryNavigationSection', start:[0,0], end:[0,0]},
+                {name: 'MainSection', start:[1,0], end:[1,0]}
+            ]}
+            gap={'small'}
+            fill
+        >
+        <Box fill background={'brand-1'} gridArea={'PrimaryNavigationSection'}>
+            {primaryNavigationContent}
+        </Box>
+        <Box fill background={'light-2'} gridArea={'MainSection'}>
+            {mainSectionContent}
+        </Box>
+        </Grid>
+    )   
 }
 
 const LayoutPortal = ({primaryNavigationContent, mainSectionContent}) => {
     return(
         <ResponsiveContext.Consumer>
             {size => (
-                size == 'small' ?
-                <PortalLayoutMobile
+                <PortalGrid
+                    size={size}
                     primaryNavigationContent={primaryNavigationContent}
                     mainSectionContent={mainSectionContent}
                 >  
-                </PortalLayoutMobile>
-                :
-                size == 'medium' ?
-                <PortalLayoutDesktop
-                    primaryNavigationContent={primaryNavigationContent}
-                    mainSectionContent={mainSectionContent}
-                >  
-                </PortalLayoutDesktop>
-                :
-                <Heading> Layout for this size is not defined </Heading>
+                </PortalGrid>
             )}
         </ResponsiveContext.Consumer>
     )   
