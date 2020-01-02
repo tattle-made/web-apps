efa12605dfe1c09213b7b83322cd2cafb9551b80
Denny George <denny.george90@gmail.com>
2020-01-02 18:12:37 +0530

    (feat) add deploy script to tattle-services (#9)

--- packages/service-ui/gatsby-config.js
+++ packages/service-ui/gatsby-config.js
@@ -42,7 +42,13 @@ module.exports = {
         },
       },
     },
-    `gatsby-plugin-styled-components`
+    `gatsby-plugin-styled-components`,
+    {
+      resolve: `gatsby-plugin-s3`,
+      options: {
+          bucketName: 'services-sandbox.tattle.co.in'
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
