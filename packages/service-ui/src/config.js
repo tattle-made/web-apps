ee41f9fdcbf6beb579a609dedb64aa3addd9e446
denny <denny.george90@gmail.com>
2020-01-31 12:29:03 +0530

    (chore) deploy

--- packages/service-ui/src/config.js
+++ packages/service-ui/src/config.js
@@ -1,3 +1,3 @@
-// export const ARCHIVE_SERVER_PATH = 'http://archive-staging.ap-south-1.elasticbeanstalk.com:3003';
-export const ARCHIVE_SERVER_PATH = 'http://localhost:3003';
+export const ARCHIVE_SERVER_PATH = 'http://archive-staging.ap-south-1.elasticbeanstalk.com:3003';
+// export const ARCHIVE_SERVER_PATH = 'http://localhost:3003';
 export const TOKEN = '7d5208f0-4295-11ea-b1cd-857f27f10814';
