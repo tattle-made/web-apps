7d0e7fabb2b55881b988c380e564aa9697a4ce9b
denny <denny.george90@gmail.com>
2020-02-19 17:27:29 +0530

    (feat) add support for local, live, prod setup

---
+++ packages/service-ui/src/config/config-prod.js
@@ -0,0 +1,2 @@
+export const ARCHIVE_SERVER_PATH = 'https://archive.tattle.co.in';
+export const TOKEN = '7d5208f0-4295-11ea-b1cd-857f27f10814';
---
+++ packages/service-ui/src/config/setup-config.sh
@@ -0,0 +1,21 @@
+echo 'Setting up Config based on environment'
+
+if [ "$1" == "DEVELOPMENT" ]
+then
+    cp src/config/config-dev.js src/config.js
+    cp src/config/gatsby-config-dev.js gatsby-config.js
+fi
+
+if [ "$1" == "PRODUCTION" ]
+then
+    cp src/config/config-prod.js src/config.js
+    cp src/config/gatsby-config-prod.js gatsby-config.js
+fi
+
+
+if [ "$1" == "LOCALHOST" ]
+then
+    cp src/config/config-local.js src/config.js
+    cp src/config/gatsby-config-local.js gatsby-config.js
+fi
+
