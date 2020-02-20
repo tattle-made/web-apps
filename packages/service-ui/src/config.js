a0366d1583d0d026214222cfe77024555822255e
denny <denny.george90@gmail.com>
2020-02-20 09:09:58 +0530

    dmp

--- packages/service-ui/src/config.js
+++ packages/service-ui/src/config.js
@@ -1,2 +1,2 @@
-export const ARCHIVE_SERVER_PATH = 'http://localhost:3003';
+export const ARCHIVE_SERVER_PATH = 'https://archive-dev.tattle.co.in';
 export const TOKEN = '7d5208f0-4295-11ea-b1cd-857f27f10814';
