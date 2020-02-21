5c82fd0fa3e769a6e30d7d7a018a09759799c4d2
denny <denny.george90@gmail.com>
2020-02-21 12:42:23 +0530

    (feat) add updated token

--- packages/service-ui/src/config/config-dev.js
+++ packages/service-ui/src/config/config-dev.js
@@ -1,2 +1,2 @@
 export const ARCHIVE_SERVER_PATH = 'https://archive-server-dev.tattle.co.in';
-export const TOKEN = '7d5208f0-4295-11ea-b1cd-857f27f10814';
+export const TOKEN = '76a98aa0-5479-11ea-8616-4977f579cd39';
