af5d1df113db827baf40a18448ed108b3051b66e
denny <denny.george90@gmail.com>
2020-02-22 01:09:37 +0530

    (feat) add version to index page of service

--- packages/service-ui/src/components/footer.js
+++ packages/service-ui/src/components/footer.js
@@ -2,14 +2,22 @@
 import { jsx } from 'theme-ui'
 import React from 'react'
 import FooterLink from './footer-link'
+import { Box, Text} from 'grommet'
+
+import {version} from '../../package.json';
 
 const Footer = props =>
 <div sx={{
     padding: 2
 }}>
-    <FooterLink url='http://blog.tattle.co.in/' label='BLOG'/> |
-    <FooterLink url='https://mailchi.mp/b0a851c89c0d/tattle_update' label='MAILING LIST'/> |
-    <FooterLink url='mailto:manage@tattle.co.in' label='CONTACT'/>
+    <Box direction={'row'} gap={'xsmall'}>
+        <FooterLink url='http://blog.tattle.co.in/' label='BLOG'/> |
+        <FooterLink url='https://mailchi.mp/b0a851c89c0d/tattle_update' label='MAILING LIST'/> |
+        <FooterLink url='mailto:manage@tattle.co.in' label='CONTACT'/>
+    </Box>
+
+    
+    <Text size={'small'} color={'gray'}> version : {version} </Text>
 </div>
 
 export default Footer;
